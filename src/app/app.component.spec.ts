b8f2b0daf9332a07bd03966c45d298a6de963871
saho0821 <sahana.honakeri@netcracker.com>
2023-06-30 14:30:55 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-project'`, () => {
+  it(`should have as title 'new-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-project');
+    expect(app.title).toEqual('new-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('new-project app is running!');
   });
 });
